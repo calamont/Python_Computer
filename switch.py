c5a3e76cd2aa62fd0e296221cab8b80c4198ad08
Callum Lamont <cal_lamont@hotmail.com>
2020-03-30 22:06:21 +0100

    Added SRLatch class

--- switch.py
+++ switch.py
@@ -1,4 +1,5 @@
 """Various switches that can be built using our logic gates."""
+from logic import NOR
 
 def D_latch():
     pass
@@ -6,5 +7,22 @@ def D_latch():
 def D_flip_flop():
     pass
 
-def SR_latch():
-    pass
+class SRLatch:
+
+    def __init__(self): 
+        self._Q = 1
+        self._inv_Q = 0
+
+    def __call__(self, set_val=0, reset_val=0):
+        # TODO: create and raise InvalidStateError if set_val and reset_val
+        # set to 1
+        
+        if set_val:
+            self._Q = NOR(set_val, self._inv_Q)
+            self._inv_Q = NOR(reset_val, self._Q)
+        if reset_val:
+            self._inv_Q = NOR(reset_val, self._Q)
+            self._Q = NOR(set_val, self._inv_Q)
+
+        return self._Q, self._inv_Q
+
