b75ea96762f012378362d3e5a768b8c8121076d2
calamont <cal_lamont@hotmail.com>
2020-07-19 06:56:43 +0100

    synchronous counter on its own

--- computer/counter.py
+++ computer/counter.py
@@ -16,7 +16,23 @@ class Counter:
         return count
 
 
-class SynchronousCounter:
+class Counter:
+    """Synchronous n-bit counter"""
+    def __init__(self, nbits):
+        self.nbits = nbits
+        self.latches = [PrimarySecondaryJKFlipFlop() for _ in range(nbits)]
+
+    def __call__(self, clock=1):
+        output = []
+        input = 1
+        for FlipFlop in self.latches:
+            Q, _ = FlipFlop(input, input, clock)
+            output.append(NOT(Q))
+            input = AND(Q, input)
+        return output
+
+
+class ProgramCounter:
 
     def __init__(self, nbits):
         self.nbits = nbits
@@ -35,11 +51,11 @@ class SynchronousCounter:
         return output
 
 
-class ProgramCounter(SynchronousCounter):
+# class ProgramCounter(SynchronousCounter):
 
-    def __init__(self, nbits):
-        super().__init__(nbits)
+#     def __init__(self, nbits):
+#         super().__init__(nbits)
 
-    def __call__(self, write, jump, clock, clear=0):
-        # Try to add clear functionality next
-        return super().__call__(AND(write, clock))
+#     def __call__(self, write, jump, clock, clear=0):
+#         # Try to add clear functionality next
+#         return super().__call__(AND(write, clock))
